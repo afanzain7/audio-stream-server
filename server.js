1db12abafbb1c707861e3e9024524334d1b24511
afanzain7 <zainafan05@gmail.com>
2025-06-13 14:02:46 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+let adminConnected = false;
+
+io.on('connection', (socket) => {
+    if (socket.handshake.query.role === 'admin') {
+        adminConnected = true;
+        io.emit('adminStatus', true);
+    }
+    socket.on('audio', (data) => {
+        if (adminConnected) socket.broadcast.emit('audio', data);
+    });
+    socket.on('disconnect', () => {
+        if (socket.handshake.query.role === 'admin') {
+            adminConnected = false;
+            io.emit('adminStatus', false);
+        }
+    });
+});
+
+server.listen(3000, '0.0.0.0', () => console.log('Server on port 3000'));
